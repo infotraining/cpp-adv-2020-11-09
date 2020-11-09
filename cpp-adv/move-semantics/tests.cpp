94ced633214d3574ece58afb05059bc5b95fa357
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-09 09:59:33 +0100

    move-semantics - 1

--- cpp-adv/move-semantics/tests.cpp
+++ cpp-adv/move-semantics/tests.cpp
@@ -9,7 +9,21 @@
 
 using namespace std;
 
-TEST_CASE("test")
+string full_name(const string& first, const string& last)
 {
-    
+    return first + " " + last;
+}
+
+TEST_CASE("reference binding")
+{
+    std::string name = "jan";
+
+    SECTION("C++98")
+    {
+        std::string& ref_name = name;
+
+        const std::string& ref_full_name = full_name(name, "Kowalski");
+        REQUIRE(ref_full_name.size() == 12);
+        //ref_full_name[0] = 'J';
+    }
 }
